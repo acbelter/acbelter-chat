fa11f70054852022dcd65c1bb2d9595c857c4038
acbelter <acbelter@gmail.com>
2015-12-15 18:41:36 +0300

    Updated chat test.

--- test/com/acbelter/chat/integration/ChatTest.java
+++ test/com/acbelter/chat/integration/ChatTest.java
@@ -6,19 +6,19 @@ import com.acbelter.chat.command.base.CommandHandler;
 import com.acbelter.chat.command.base.CommandType;
 import com.acbelter.chat.jdbc.MessageDatabaseStore;
 import com.acbelter.chat.jdbc.UserDatabaseStore;
-import com.acbelter.chat.message.ChatCreateMessage;
-import com.acbelter.chat.message.ChatListMessage;
-import com.acbelter.chat.message.LoginMessage;
+import com.acbelter.chat.message.*;
 import com.acbelter.chat.message.base.Message;
 import com.acbelter.chat.message.base.MessageStore;
 import com.acbelter.chat.message.base.User;
 import com.acbelter.chat.message.base.UserStore;
 import com.acbelter.chat.message.result.ChatCreateResultMessage;
+import com.acbelter.chat.message.result.ChatHistoryResultMessage;
 import com.acbelter.chat.message.result.ChatListResultMessage;
 import com.acbelter.chat.message.result.LoginResultMessage;
 import com.acbelter.chat.net.*;
 import com.acbelter.chat.net.nio.NioClient;
 import com.acbelter.chat.net.nio.NioServer;
+import org.apache.commons.lang.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,7 +31,7 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
-public class CreateChatTest {
+public class ChatTest {
     private NioServer server;
     private NioClient firstClient;
     private NioClient secondClient;
@@ -114,6 +114,7 @@ public class CreateChatTest {
 
     @Test
     public void createChat() throws Exception {
+        // Логин первого пользователя
         firstClientHandler.send(new LoginMessage("test", "1234"));
         Thread.sleep(1000);
         assert firstResultMessage.getType() == CommandType.LOGIN_RESULT;
@@ -123,6 +124,7 @@ public class CreateChatTest {
         firstClientHandler.send(new ChatCreateMessage(firstLoginMsg.getUserId()));
         Thread.sleep(1000);
 
+        // Логин второго пользователя
         secondClientHandler.send(new LoginMessage("user", "123"));
         Thread.sleep(1000);
         assert secondResultMessage.getType() == CommandType.LOGIN_RESULT;
@@ -133,21 +135,44 @@ public class CreateChatTest {
         userIdList.add(firstLoginMsg.getUserId());
         userIdList.add(secondLoginMsg.getUserId());
 
+        // Первый пользователь создает чат
         firstClientHandler.send(new ChatCreateMessage(userIdList));
         Thread.sleep(1000);
 
         assert firstResultMessage.getType() == CommandType.CHAT_CREATE_RESULT;
         ChatCreateResultMessage chatCreateMsg = (ChatCreateResultMessage) firstResultMessage;
+        Long newChatId = chatCreateMsg.getNewChatId();
 
         ChatListMessage getChatListMsg = new ChatListMessage();
         getChatListMsg.setSender(secondLoginMsg.getUserId());
         secondClientHandler.send(new ChatListMessage());
         Thread.sleep(5000);
 
+        // Второй пользователь получает список чатов
         assert secondResultMessage.getType() == CommandType.CHAT_LIST_RESULT;
         ChatListResultMessage chatListMsg = (ChatListResultMessage) secondResultMessage;
-        assert chatListMsg.getChatData().containsKey(chatCreateMsg.getNewChatId());
-        assert chatListMsg.getChatData().get(chatCreateMsg.getNewChatId()).containsAll(userIdList);
+        assert chatListMsg.getChatData().containsKey(newChatId);
+        assert chatListMsg.getChatData().get(newChatId).containsAll(userIdList);
+
+        // Первый пользователь отправляет сообщение второму пользователю в созданный чат
+        String testMessage = RandomStringUtils.randomAscii(16);
+        firstClientHandler.send(new ChatSendMessage(newChatId, testMessage));
+        Thread.sleep(1000);
+
+        // Второй пользователь получает сообщение от первого пользователя
+        assert secondResultMessage.getType() == CommandType.CHAT_SEND;
+        ChatSendMessage chatSendMsg = (ChatSendMessage) secondResultMessage;
+        assertEquals(testMessage, chatSendMsg.getMessage());
+
+        // Первый пользователь получает историю сообщений
+        firstClientHandler.send(new ChatHistoryMessage(newChatId));
+        Thread.sleep(1000);
+
+        assert firstResultMessage.getType() == CommandType.CHAT_HISTORY_RESULT;
+        ChatHistoryResultMessage chatHistoryMsg = (ChatHistoryResultMessage) firstResultMessage;
+
+        List<String> history = chatHistoryMsg.getMessages();
+        assert history.get(history.size() - 1).contains(testMessage);
     }
 
     @After
